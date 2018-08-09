921ecead727790ed24ba4d4f5d6e4412ca35bf30
Hugues Kamba <hugues.kamba@arm.com>
2018-08-09 08:40:31 +0100

    IOTMBL-600 Add more test cases for the random number generator
    * Run the rngtest tool from the Python script to perform the FIPS 140-2
    test.
    * Add a compression test to check the lack of redundancy in the zipped
    file.
    * Refactor and rename test case that uses chi square to test the null
    hypothesis.
    * Add more Python required libraries.

--- mbl-core/test/devices/random/test_random_number_generator.py
+++ mbl-core/test/devices/random/test_random_number_generator.py
@@ -1,76 +1,157 @@
 # Copyright (c) 2018 ARM Ltd.
 
 
+import subprocess
+import re
 import os
 import scipy.stats
 import numpy
+import zipfile
 
 """ Pytest tests
 
-Pytest is the framework used to run tests. It autodiscovers tests by the name
-of the class or method/function. More info about autodiscovery:
+Pytest is the framework used to run tests. It autodiscovers tests by the
+name of the class or method/function. More info about autodiscovery:
 https://docs.pytest.org/en/latest/goodpractices.html#test-discovery
 
 Pytest documentation can be found here: https://docs.pytest.org/en/latest/
 
 The aim of this file is to test the randomness of the OS random generator.
+
 This is done by reading the special file /dev/random which serves as
 pseudorandom number generator.
 
 """
-DEFAULT_RANDOMNESS_SOURCE = "/dev/random"
+RANDOMNESS_SOURCE = "/dev/random"
+
+# helper function(s)
+
+
+def extract_count_from_str(
+    stats_str: str,
+    stats_preceding_str: str
+) -> str:
+    """ Extracts the count from a line of a rngtest statistics output."""
+    count = re.search(
+        pattern="{}(.*)$".format(stats_preceding_str),
+        string=stats_str
+    ).group(1)
+    return count
+
+
+def run_chi_squared_goodness_of_fit(
+) -> (numpy.float64, numpy.float64):
+    """Perform Hypothesis testing to determine with a certain
+       level of certainty that the hypothesis that the data
+       is taken from a uniform distribution can be rejected.
+       Return the chi square and the p-value in that order.
+    """
+    # Number of possible numbers or cells or bins is 256 because a
+    # random byte between 0 and 255 is generated.
+    num_cell = 256
+
+    # Number of times the rng will be run.
+    no_of_random_bytes = 25600
+
+    # Create a random number generator object.
+    rng = RandomNumberGeneratorService()
+
+    # Create a list that will contain the number of occurrences for
+    # each number between 0 - 255 i.e cells[n] contains the number of
+    # occurences of n.
+    cells = []
+
+    # Initialise the list with zeros.
+    for _ in range(num_cell):
+        cells.append(0)
+
+    # Set the occurrences of each numbers between 0 and 255.
+    for n in range(0, no_of_random_bytes):
+        cells[rng.generate_random_byte()] += 1
+
+    # Calculate the chisquare and p-value.
+    chi2, p = scipy.stats.chisquare(f_obs=cells)
+    return (chi2, p)
 
 
 class RandomNumberGeneratorService():
-    """The service class to generate random numbers."""
+    """The service class to generate random bytes."""
 
     def __init__(
         self,
-        source: str = DEFAULT_RANDOMNESS_SOURCE
+        source: str = RANDOMNESS_SOURCE
     ) -> int:
-        self.numbers = []
         self.set_bytes_generator(source)
 
     def set_bytes_generator(
         self,
-        source: str = DEFAULT_RANDOMNESS_SOURCE
+        source: str = RANDOMNESS_SOURCE
     ):
         self.src = source
         self.bytes_generator = open(source, "rb")
 
-    def generate_random_int(
+    def generate_random_byte(
         self
     ) -> list:
-        """Generate an integer value."""
+        """Generate a byte."""
         return int.from_bytes(
             self.bytes_generator.read(1),
             byteorder='big'
         )
 
-    def generate_n_random_ints(
+    def generate_n_random_bytes(
         self,
-        n: int = 1
+        n: int=1
     ):
-        """Generate a number of integers."""
+        """Generate a number of bytes."""
+        bytes_ls = []
         while len(self.numbers) < n:
-            number = self.generate_random_int()
-            self.numbers.append(number)
+            byte = self.generate_random_byte()
+            bytes_ls.append(byte)
+        return bytes_ls
 
 
 class TestRandomNumberGenerator():
-    """Test the random number generator."""
+    """Test method(s) for the random number generator."""
 
-    def test_random_streams_generated(
+    def test_compression_size(
         self
     ):
-        """The test ensures that the random number generator produces
-           a random stream. This is done by running an hypothesis test
-           multiple times and analysing the probability of observing a test
-           statistic at least as extreme in a chi-squared distribution, also
-           known as p-value.
+        """Test the randomness of the data by ensuring that no redundancy is
+           detected after compressing a file containing a number of bytes
+           obtained from the random generator source.
+        """
+        # Create file to zip
+        unzipped_file = "random_bytes.txt"
+        no_of_bytes = 4000
+        rng = RandomNumberGeneratorService()
+        f = open(unzipped_file, "wb+")
+        for _ in range(0, no_of_bytes):
+            f.write(rng.bytes_generator.read(1))
+        f.close()
+
+        # Zip file
+        zipped_file = "random_bytes.zip"
+        zipfile.ZipFile(zipped_file, mode='w').write(unzipped_file)
+
+        # Check result
+        assert os.path.getsize(zipped_file) >= os.path.getsize(unzipped_file)
+
+        # Clean up
+        os.remove(unzipped_file)
+        os.remove(zipped_file)
+
+    def test_null_hypothesis(
+        self
+    ):
+        """The test performs hypothesis testing to determine with a certain
+           level of certainty that the hypothesis that the data
+           is taken from a uniform distribution can be rejected. This is done
+           by running an hypothesis test multiple times and analysing the
+           probability of observing a test statistic at least as extreme in
+           a chi-squared distribution, also known as p-value.
            A pass is determined when the majority of p-values fall between
-           chosen low cutoff and high
-           cutoff value
+           chosen low cutoff and high cutoff value.
         """
         # Set up test parameters
         low_cutoff = 0.05
@@ -78,47 +159,65 @@ class TestRandomNumberGenerator():
         num_of_static_test_run = 100
         count_p_value_acceptable = 0
 
-        #
         for _ in range(num_of_static_test_run):
-            p_value = (self.run_chi_squared_goodness_of_fit_on_rng()[1])
+            p_value = (run_chi_squared_goodness_of_fit()[1])
             if not (p_value <= low_cutoff) and not (p_value >= high_cutoff):
                 count_p_value_acceptable += 1
 
         # Check the results.
         assert count_p_value_acceptable >= (num_of_static_test_run/2)
 
-    # helper functions
-    def run_chi_squared_goodness_of_fit_on_rng(
+    def test_fips_140_2(
         self
-    ) -> (numpy.float64, numpy.float64):
-        """Perform Hypothesis testing to determine with a certain
-           level of certainty that the hypothesis that the data
-           is taken from a uniform distribution can be rejected.
-           Return the chi square and the p-value in that order.
+    ):
+        """The test works on blocks of 20000 bits (2500 bytes), using the
+           FIPS 140-2 RNG tests (Monobit test, Poker test, Runs test,
+           Long run test and Continuous test) to test the randomness of each
+           block of data.
         """
-        # Number of possible numbers or cells or bins is 256 because a
-        # random byte between 0 and 255 is generated.
-        num_cell = 256
+        num_of_blocks = 5
+        randomness_source = "/dev/random"
 
-        # Create a random number generator object.
-        rng = RandomNumberGeneratorService()
+        # Run Linux 'rngtest' program as a shell subprocess to perform the test
+        bash_command = "rngtest -c{} < {}".format(
+            num_of_blocks,
+            randomness_source
+        )
+        process = subprocess.run(
+            args=bash_command,
+            shell=True,
+            stderr=subprocess.PIPE
+        )
 
-        # Run the random number generator multiple time.
-        rng.generate_n_random_ints(n=num_cell*100)
+        # Read the program statistics output from stderr
+        statistics_output = process.stderr.decode('utf8')
 
-        # Create a list that will contain the number of occurrences for
-        # each number between 0 - 255 i.e cells[n] contains the number of
-        # occurences of n.
-        cells = []
+        # Identify the lines that provide information about
+        # success(es) and failure(s)
+        statistics_line_success = []
+        statistics_line_failure = []
+        substr_success = "rngtest: FIPS 140-2 successes: "
+        substr_failure = "rngtest: FIPS 140-2 failures: "
+        for line in statistics_output.split('\n'):
+            if substr_success in line:
+                statistics_line_success = line
+            elif substr_failure in line:
+                statistics_line_failure = line
 
-        # Initialise the list with zeros.
-        for _ in range(num_cell):
-            cells.append(0)
+        # Get the success and failure counts
+        count_success = int(
+            extract_count_from_str(
+                stats_str=statistics_line_success,
+                stats_preceding_str=substr_success
+            )
+        )
+        count_failure = int(
+            extract_count_from_str(
+                stats_str=statistics_line_failure,
+                stats_preceding_str=substr_failure
+            )
+        )
 
-        # Set the occurrences of each numbers between 0 and 255.
-        for n in range(0, len(rng.numbers)):
-            cells[rng.numbers[n]] += 1
-
-        # Calculate the chisquare and p-value.
-        chi2, p = scipy.stats.chisquare(f_obs=cells)
-        return (chi2, p)
+        # Check the results
+        assert count_success == num_of_blocks
+        assert count_failure == 0
