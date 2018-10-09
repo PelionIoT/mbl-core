5b03097e175bf7be222c688c22b3d70c6c294a0a
Hugues Kamba <hugues.kamba@arm.com>
2018-10-09 13:54:10 +0100

    IOTMBL-734 Remove dependency on SciPy to run RNG system test (#7)
    IOTMBL-734 Remove dependency on SciPy to run RNG system test
    
    The system test case that depends on SciPy was removed as there is no
    recipe available on OE core. Furthermore, writing the recipe resulted in
    multiple dependencies that could not be resolved in the time allocated for
    the task. The test case has been removed from the main branch and
    moved to the hk-add-null-hypothesis-test-case branch so it may be restored
    when a recipe for SciPy becomes available.

--- mbl-core/tests/devices/random/test_random_number_generator.py
+++ mbl-core/tests/devices/random/test_random_number_generator.py
@@ -7,10 +7,9 @@ SPDX-License-Identifier: Apache-2.0
 import sys
 import subprocess
 import os
-import scipy.stats
-import numpy
 import gzip
 import tempfile
+
 
 """The aim of this file is to test the randomness of the OS random generator.
 
@@ -47,40 +46,6 @@ def parse_rngtest_output(rngtest_output: str) -> (int, int):
         raise
 
 
-def run_chi_squared_goodness_of_fit() -> (numpy.float64, numpy.float64):
-    """Calculate the chi-square and p-value for a rng sample.
-
-    Perform Hypothesis testing to determine with a certain level of certainty
-    that the hypothesis that the random number generator data is taken from a
-    uniform distribution can be rejected.
-    Return the chi square and the p-value in that order.
-    """
-    # Number of possible numbers or cells or bins is 256 because a
-    # random byte between 0 and 255 is generated.
-    num_cell = 256
-
-    # Number of times the rng will be run.
-    no_of_random_bytes = 25600
-
-    with RandomNumberGeneratorAccess() as rng:
-        # Create a list that will contain the number of occurrences for
-        # each number between 0 - 255 i.e cells[n] contains the number of
-        # occurences of n.
-        cells = []
-
-        # Initialise the list with zeros.
-        cells = [0] * num_cell
-
-        # Set the occurrences of each numbers between 0 and 255.
-        for n in range(0, no_of_random_bytes):
-            integer = int.from_bytes(rng.read(1), byteorder="big")
-            cells[integer] += 1
-
-        # Calculate the chisquare and p-value.
-        chi2, p = scipy.stats.chisquare(f_obs=cells)
-        return (chi2, p)
-
-
 class RandomNumberGeneratorAccess:
     """Allow safe access to random generator with the 'with' statement."""
 
@@ -110,44 +75,6 @@ class TestRandomNumberGenerator:
                     no_of_bytes = 4000
                     zipped_tmpfile.write(rng.read(no_of_bytes))
             assert os.path.getsize(zipped_tmpfile.name) >= no_of_bytes
-
-    def test_distribution_null_hypothesis(self):
-        """
-        Run multiple chi-square tests on a rng to assess its distribution.
-
-        The test performs hypothesis testing to determine with a certain
-        level of certainty that the hypothesis that the data is taken from a
-        uniform distribution can be rejected. This is done by running an
-        hypothesis test multiple times and analysing the probability of
-        observing a test statistic at least as extreme in a chi-squared
-        distribution, also known as p-value.A pass is determined when the
-        majority of p-values fall between chosen low cutoff and high cutoff
-        value. The low cutoff in this case is the significance level, which is
-        the level of p-value required to reject the null hypothesis. Typically
-        researchers choose a significance level equal to 0.01, 0.05, or 0.10;
-        0.05 was chosen here. The high cutoff value (corresponding to a low
-        chi-square value) indicates a high correlation between the sample of
-        the rng and a uniform distribution. Researchers usually use 0.95 as the
-        high cutoff value.
-        The total number of "good" p-values required to pass was set to 78% as
-        the probability that a TRNG would fail (based on chosen low and high
-        cutoff values) is 0.00011415631973171919. A TRNG would be expected to
-        to fail the test about once every 10,100 runs.
-        """
-        # Set up test parameters
-        low_cutoff = 0.05
-        high_cutoff = 0.95
-        num_of_static_test_run = 100
-        num_acceptable_p_value_for_pass = 78
-        count_p_value_acceptable = 0
-
-        for _ in range(num_of_static_test_run):
-            p_value = run_chi_squared_goodness_of_fit()[1]
-            if low_cutoff <= p_value <= high_cutoff:
-                count_p_value_acceptable += 1
-
-        # Check the results.
-        assert count_p_value_acceptable >= num_acceptable_p_value_for_pass
 
     def test_fips_140_2(self):
         """
