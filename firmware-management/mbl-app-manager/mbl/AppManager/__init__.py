8e3fe1a78327893bbdccb4111415765ba7656458
Hugues Kamba <hugues.kamba@arm.com>
2018-10-15 11:59:43 +0100

    TO BE SQUASHED: Remove content of __init__.py files

--- firmware-management/mbl-app-manager/mbl/AppManager/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
-#
-# SPDX-License-Identifier: Apache-2.0
-
-"""__init__.py file for MBL AppManager package."""
-
-name = "AppManager"
