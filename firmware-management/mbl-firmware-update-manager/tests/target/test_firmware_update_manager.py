25063ce8f1f66540b536720e2dd141dbf5d0ce95
Hugues Kamba <hugues.kamba@arm.com>
2018-10-26 19:15:33 +0100

    IOTMBL-925 Restructure mbl-firmware-update-manager dir for implicit namespace
    * Create setup.py so installation can be done with setuptools.
    * Add test for testing import using the mbl namespace.

---
+++ firmware-management/mbl-firmware-update-manager/tests/target/test_firmware_update_manager.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Pytest for testing MBL Firmware Update Manager."""
+
+import importlib
+
+
+class TestMblFirmwareUpdateManager:
+    """MBL Firmware Update Manager main class."""
+
+    def test_firmware_manager_mbl_subpackage(self):
+        """
+        Test that FirmwareUpdateManager is a subpackage of the "mbl" package.
+
+        The FirmwareUpdateManager subpackage should be accessible via the "mbl"
+        namespace.
+        """
+        # Assert that the package can be imported as a subpackage to
+        assert importlib.util.find_spec("mbl.FirmwareUpdateManager") is not None
