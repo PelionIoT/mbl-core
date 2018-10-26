91b07c01ad706e2c961d8e414778495b9ef76940
Hugues Kamba <hugues.kamba@arm.com>
2018-10-26 17:51:47 +0100

    IOTMBL-925 Restructure mbl-app-update-manger dir for implicit namespace
    * Create setup.py so installation can be done with setuptools.
    * Add test for testing import using mbl namespace.

---
+++ firmware-management/mbl-app-update-manager/tests/target/test_app_update_manager.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Pytest for testing MBL App Update Manager."""
+
+import importlib
+
+
+class TestMblAppUpdateManager:
+    """MBL App Update Manager main class."""
+
+    def test_app_manager_mbl_subpackage(self):
+        """
+        Test that AppUpdateManager is a subpackage of the "mbl" package.
+
+        The AppUpdateManager subpackage should be accessible via the "mbl"
+        namespace.
+        """
+        # Assert that the package can be imported as a subpackage to
+        assert importlib.util.find_spec("mbl.AppUpdateManager") is not None
