fd5fb15a23a54f8e80d104cb5d0235c2c8d85798
Yelena Gordon <yelena.gordon@arm.com>
2018-11-06 17:59:05 +0200

    IOTMBL-675: hello world C application (#25)
    * IOTMBL-675: hello world C application
    
    * IOTMBL-675: hello world C application - PR
    
    * IOTMBL-675: hello world C application - PR
    
    * IOTMBL-675: hello world C application - PR
    
    * IOTMBL-675: hello world C application - PR
    
    * IOTMBL-675: hello world C application - PR
    
    * IOTMBL-675: hello world C application - PR
    
    * IOTMBL-675: hello world C application - PR

---
+++ tutorials/helloworld/src/hello_world.c
@@ -0,0 +1,21 @@
+/***************************************************/
+/* User sample application printing "Hello, world" */
+/***************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+
+#define NUM_OF_PRINTS 10
+#define DELAY_SEC_BETWEEN_PRINTS 2
+
+int main (void)
+{
+  int i;
+  for(i=0; i < NUM_OF_PRINTS; ++i)
+  {
+      printf("Hello, world\n");
+      sleep(DELAY_SEC_BETWEEN_PRINTS);
+  }
+  
+  return 0;
+}
