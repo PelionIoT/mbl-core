ff70aa29b2a2e6e860f82531ba8dd1df5ce5b614
Hugues Kamba <hugues.kamba@arm.com>
2018-12-12 09:05:38 +0000

    Set BSD-3-Clause license in setup() function call of setup.py files (#47) (#48)
    Also remove empty author email argument.

--- application-framework/mbl-app-lifecycle-manager/setup.py
+++ application-framework/mbl-app-lifecycle-manager/setup.py
@@ -20,8 +20,7 @@ setup(
     description="Mbed Linux OS Application Lifecycle Manager",
     long_description=read("README.md"),
     author="Arm Ltd.",
-    author_email="",
-    license="Apache-2.0",
+    license="BSD-3-Clause",
     packages=["mbl.app_lifecycle_manager"],
     zip_safe=False,
     entry_points={
