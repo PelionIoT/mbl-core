2092137a1bf9c538424c1db3b1343a382c7e3923
Yelena Gordon <yelena.gordon@arm.com>
2019-01-16 11:19:40 +0200

    Connect to D-Bus from Python sample code (#57)
    * Connect to D-Bus from Python sample code
    
    Task: IOTMBL-1395
    
    * Implement Python sample code that will connect
      and register to the Session D-Bus using pydbus
      binding. This is an application side.
    * Implement implement pytest application life cycle
       base class and application connectivity test class.
       Application life cycle test is responsible for start/stop
       a sample application.

---
+++ cloud-services/mbl-cloud-client/tests/mbl-app-lifecycle/mbl/app_lifecycle/app_lifecycle.py
@@ -0,0 +1,147 @@
+# !/usr/bin/env python3
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+D-Bus test application infrastructure script.
+
+This script simulate application behavior for tests: application start, stop
+and D-Bus connectivity check.
+"""
+
+import sys
+import os
+import logging
+from enum import Enum
+from pydbus.generic import signal
+from pydbus import SessionBus
+from gi.repository import GLib
+
+
+__version__ = "1.0"
+DBUS_STOP_SIGNAL_FILTER = "/mbl/app/test1/stop"
+DEFAULT_DBUS_NAME = "mbl.app.test1"
+
+
+class ReturnCode(Enum):
+    """AppLifeCycle error codes."""
+
+    SUCCESS = 0
+    ERR_OPERATION_FAILED = 1
+
+
+class AppLifeCycle:
+    """Application life cycle."""
+
+    dbus = """
+<node name='/mbl/app/test1'>
+    <interface name='mbl.app.test1'>
+        <method name='GetPid'>
+            <arg type='n' name='pid' direction='out'/>
+        </method>
+    </interface>
+</node>
+    """
+
+    bus_main_loop = GLib.MainLoop()
+
+    def __init__(self):
+        """Create AppLifeCycle object."""
+        self.logger = logging.getLogger("mbl-app-lifecycle")
+        self.logger.info(
+            "Creating AppLifeCycle version {}".format(__version__)
+        )
+        self.all_methods_for_publish_on_dbus = []
+
+    def AddToPublish(self, methods_for_publish_on_dbus):
+        """Add object interfaces to the data to be published on D-Bus."""
+        self.all_methods_for_publish_on_dbus.append(
+            methods_for_publish_on_dbus
+        )
+        self.logger.debug(
+            "The following interfaces will be published on D-Bus: {}".format(
+                methods_for_publish_on_dbus
+            )
+        )
+
+    def Run(self):
+        """Run Main loop."""
+        self.logger.info("Connect D-Bus: {}".format(DEFAULT_DBUS_NAME))
+
+        bus = SessionBus()
+        self.logger.debug(
+            "Publishing following interfaces on D-Bus: {}".format(
+                *(self.all_methods_for_publish_on_dbus)
+            )
+        )
+        self.obj = bus.publish(
+            DEFAULT_DBUS_NAME,
+            AppLifeCycle(),
+            *(self.all_methods_for_publish_on_dbus)
+        )
+
+        # subscribe to the Stop signal:
+        # object - object path to match on or None to match on all object
+        # paths; signal_fired - callable, invoked when there is a signal
+        # matching the requested data. For more information about subscribe()
+        # method please refer Github:
+        # https://github.com/LEW21/pydbus/blob/master/pydbus/subscription.py
+        bus.subscribe(
+            object=DBUS_STOP_SIGNAL_FILTER, signal_fired=self.StopSignal
+        )
+
+        self.logger.info("Running main loop...")
+        AppLifeCycle.bus_main_loop.run()
+
+    def GetPid(self):
+        """
+        Get process Id D-Bus test method.
+
+        :return: process Id
+        """
+        pid = os.getpid()
+        self.logger.debug("Application process ID {}".format(pid))
+        return pid
+
+    def StopSignal(self, *args):
+        """Stop D-Bus main loop."""
+        self.logger.info("Got stop signal, quitting the D-Bus main loop")
+        AppLifeCycle.bus_main_loop.quit()
+
+
+class AppConnectivity(AppLifeCycle):
+    """Checks connectivity of caller to the application through the D-Bus."""
+
+    dbus = """
+<node name='/mbl/app/test1/AppConnectivity1'>
+    <interface name='mbl.app.test1.AppConnectivity1'>
+        <method name='Hello'>
+            <arg type='s' name='result' direction='out'/>
+        </method>
+    </interface>
+</node>
+    """
+
+    def __init__(self):
+        """Create AppConnectivity object."""
+        AppLifeCycle.__init__(self)
+        self.logger.debug("__init__")
+
+    def Run(self):
+        """Publish methods on D-Bus and run D-Bus main loop."""
+        self.logger.info("AppConnectivity Run")
+        AppLifeCycle.AddToPublish(
+            self, methods_for_publish_on_dbus=("AppConnectivity1", self)
+        )
+        AppLifeCycle.Run(self)
+
+    def Hello(self):
+        """
+        Connectivity check D-Bus method.
+
+        :return: 'Hello!'
+        """
+        output = "Hello!"
+        self.logger.info("{} request processed".format(output))
+        return output
---
+++ cloud-services/mbl-cloud-client/tests/mbl-app-lifecycle/tests/target/test_mbl-app-lifecycle.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Pytest for testing mbl application connectivity to Cloud Client."""
+
+import os
+import sys
+import importlib
+import subprocess
+import time
+from enum import Enum
+from pydbus.generic import signal
+from pydbus import SessionBus
+from gi.repository import GLib
+
+
+DEFAULT_DBUS_NAME = "mbl.app.test1"
+DBUS_OBJECT_PATH_APP_CONNECTIVITY1 = "/mbl/app/test1/AppConnectivity1"
+DBUS_STOP_SIGNAL = "mbl.app.test1.stop"
+
+DBUS_SERVICE_PUBLISHING_TIME = 1
+DBUS_SERVICE_PUBLISHING_WAIT_MAX_RETRIES = 15
+APP_LIFECYCLE_PROCESS_TERMINATION_TIMEOUT = 15
+
+
+class TstReturnCode(Enum):
+    """Test ReturnCode error codes."""
+
+    TEST_SUCCESS = 0
+    TEST_FAILED = 1
+
+
+class TestAppConnectivity:
+    """Test application connectivity main class."""
+
+    """
+    TestAppConnectivity definition.
+    Emit / Publish a application stop signal
+    """
+    dbus = """
+    <node>
+        <interface name="mbl.app.test1.stop">
+            <signal name="stop_signal">
+            </signal>
+        </interface>
+    </node>
+    """
+
+    stop_signal = signal()
+
+    def setup_method(self, method):
+        """Setup any state specific to the execution of the given method."""
+        print("Setup method TestAppConnectivity...")
+
+        # get the session bus
+        self.bus = SessionBus()
+        self.obj = self.bus.publish(DBUS_STOP_SIGNAL, self)
+
+        command = ["mbl-app-lifecycle", "-v"]
+        print("Executing command: {}".format(command))
+
+        self.proc = subprocess.Popen(command)
+        print("Finish executing command: {}".format(command))
+
+        # time out to enable service to be published on the D-Bus
+        for timeout in range(DBUS_SERVICE_PUBLISHING_WAIT_MAX_RETRIES):
+            try:
+                the_object = self.bus.get(
+                    DEFAULT_DBUS_NAME,
+                    object_path=DBUS_OBJECT_PATH_APP_CONNECTIVITY1,
+                )
+                break
+            except GLib.GError:
+                print(
+                    "Waiting for dbus object publication {} seconds".format(
+                        timeout
+                    )
+                )
+                time.sleep(DBUS_SERVICE_PUBLISHING_TIME)
+
+        print("Setup method TestAppConnectivity end")
+
+    def teardown_method(self, method):
+        """Teardown method: stop the D-Bus main loop."""
+        print("Teardown method TestAppConnectivity start...")
+
+        print("Stopping mbl-app-lifecycle main loop")
+        # send stop signal
+        self.stop_signal()
+
+        try:
+            print("Waiting for App Pydbus process to terminate.")
+            returncode = self.proc.wait(
+                timeout=APP_LIFECYCLE_PROCESS_TERMINATION_TIMEOUT
+            )
+            assert (
+                returncode == 0
+            ), "Wait for App Pydbus process to terminate returned code {} is"
+            " not 0".format(returncode)
+
+            print("Process wait returncode: {}".format(returncode))
+
+        except subprocess.TimeoutExpired:
+            print(
+                "TimeoutExpired for process wait for termination, killing "
+                "mbl-app-lifecycle process"
+            )
+            self.proc.kill()
+            out_app_lifecycle, err_app_lifecycle = self.proc.communicate()
+            print(
+                "Process communicate stdout: {}, stderr: {}".format(
+                    out_app_lifecycle, err_app_lifecycle
+                )
+            )
+            assert 0, "Wait for process terminate: TimeoutExpired"
+
+        print("Teardown method TestAppConnectivity end")
+
+    def test_app_hello(self):
+        """Connectivity test: through the D-Bus call Hello method."""
+        # get the object
+        the_object = self.bus.get(
+            DEFAULT_DBUS_NAME, object_path=DBUS_OBJECT_PATH_APP_CONNECTIVITY1
+        )
+        # call Hello method
+        result = the_object.Hello()
+        assert (
+            result == "Hello!"
+        ), "Hello method call on D-Bus returned wrong value: {}".format(result)
+        print("Hello method call on D-Bus returned: {}".format(result))
