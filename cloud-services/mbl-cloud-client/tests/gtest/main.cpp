eb13e3157208512789b863ba0d42cb6d47560114
Yelena Gordon <yelena.gordon@arm.com>
2019-02-05 17:58:16 +0200

    Gtest - temp solution. - cont

--- cloud-services/mbl-cloud-client/tests/gtest/main.cpp
+++ cloud-services/mbl-cloud-client/tests/gtest/main.cpp
@@ -4,10 +4,28 @@
 * SPDX-License-Identifier: BSD-3-Clause
 */
 
+#include "mbed_trace.h"
 #include <gtest/gtest.h>
+#include "MblCloudClient.h"
+
+#include MBED_CLOUD_CLIENT_USER_CONFIG_FILE
+
+M2MObjectList m2m_object_list;
+
+M2MBase::Mode mode;
+M2MBase::Operation operation;
+M2MResourceBase::ResourceType type;
+
+namespace mbl {
+
+
+
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv); 
+    
+    tr_debug("test 123");
     return RUN_ALL_TESTS();
 }
 
