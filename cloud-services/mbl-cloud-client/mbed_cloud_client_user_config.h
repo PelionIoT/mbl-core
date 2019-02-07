0918c745e9822617067eb33ec53f34711caedb41
Jonathan Haigh <jonathan.haigh@arm.com>
2019-02-07 14:17:33 +0000

    mbl-cloud-client: make dev features configurable
    For IOTMBL-1534: Add eval image type to MBL and enable mbl-master builds
    
    We are changing the way certificates for connecting to the Pelion cloud
    and authenticating firmware updates are provisioned.
    
    Currently, these certificates are built directly into the
    mbl-cloud-client binary. This approach is not suitable any more because
    we want to publish pre-built MBL disk images, and we don't want these
    images to contain sensitive certificate data.
    
    Instead, we are going to build mbl-cloud-client without certificates and
    provide a tool to provision devices with certificates at run time.
    
    The code to build in certificates is already protected by various #ifdefs so
    just make the relevant macros configurable on the CMake command line.

--- cloud-services/mbl-cloud-client/mbed_cloud_client_user_config.h
+++ cloud-services/mbl-cloud-client/mbed_cloud_client_user_config.h
@@ -30,7 +30,4 @@
 /* set download buffer size in bytes (min. 1024 bytes) */
 #define MBED_CLOUD_CLIENT_UPDATE_BUFFER          (2 * 1024 * 1024)
 
-#define MBED_CLOUD_DEV_UPDATE_CERT
-#define MBED_CLOUD_DEV_UPDATE_ID
-
 #endif /* MBED_CLOUD_CLIENT_USER_CONFIG_H */
