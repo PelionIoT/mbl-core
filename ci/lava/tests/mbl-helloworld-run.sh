db8877c640fdff522d86915de01003eb2a11ab81
Pete Dyer <pete.dyer@arm.com>
2019-02-13 14:29:07 +0000

    Mover the -a to the start rather than the end.

--- ci/lava/tests/mbl-helloworld-run.sh
+++ ci/lava/tests/mbl-helloworld-run.sh
@@ -12,29 +12,29 @@ dut_address=`grep "mbed-linux-os" device_list | cut -d":" -f3-`
 rm device_list
 
 # Initially attempt to cleanup any previous runs
-mbl-cli shell 'mbl-app-manager -r user-sample-app-package' -a $dut_address
-mbl-cli shell 'rm /home/app/user-sample-app-package_1.0_armv7vet2hf-neon.ipk' -a $dut_address
-mbl-cli shell 'rm /var/log/app/user-sample-app-package.log' -a $dut_address
+mbl-cli -a $dut_address shell 'mbl-app-manager -r user-sample-app-package'
+mbl-cli -a $dut_address shell 'rm /home/app/user-sample-app-package_1.0_armv7vet2hf-neon.ipk'
+mbl-cli -a $dut_address shell 'rm /var/log/app/user-sample-app-package.log'
 
 # Now install the package - this should cause it to run
-mbl-cli put /home/ubuntu/user-sample-app-package_1.0_armv7vet2hf-neon.ipk /home/app -a $dut_address
-mbl-cli shell 'mbl-app-manager -i /home/app/user-sample-app-package_1.0_armv7vet2hf-neon.ipk' -a $dut_address
+mbl-cli -a $dut_address put /home/ubuntu/user-sample-app-package_1.0_armv7vet2hf-neon.ipk /home/app
+mbl-cli -a $dut_address shell 'mbl-app-manager -i /home/app/user-sample-app-package_1.0_armv7vet2hf-neon.ipk'
 
 # Check it is there
-mbl-cli shell 'mbl-app-manager -l' -a $dut_address
+mbl-cli -a $dut_address shell 'mbl-app-manager -l'
 
 # The app takes 20 seconds to run, so wait for 30
 sleep 30
 
 # Extract the log file from the device
-mbl-cli get /var/log/app/user-sample-app-package.log /home/ubuntu/ -a $dut_address
+mbl-cli -a $dut_address get /var/log/app/user-sample-app-package.log /home/ubuntu/
 
 # Echo it to the test run
 cat /home/ubuntu/user-sample-app-package.log
 
 # Attempt to cleanup anfter the run
-#mbl-cli shell 'mbl-app-manager -r user-sample-app-package' -a $dut_address
-#mbl-cli shell 'rm /home/app/user-sample-app-package_1.0_armv7vet2hf-neon.ipk' -a $dut_address
-#mbl-cli shell 'rm /var/log/app/user-sample-app-package.log' -a $dut_address
+#mbl-cli -a $dut_address shell 'mbl-app-manager -r user-sample-app-package'
+#mbl-cli -a $dut_address shell 'rm /home/app/user-sample-app-package_1.0_armv7vet2hf-neon.ipk'
+#mbl-cli -a $dut_address shell 'rm /var/log/app/user-sample-app-package.log'
 
 
