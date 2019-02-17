354cb7a48010b3135837f075a687d454c63d1673
Dmitry Grinberg <Dmitry.Grinberg@arm.com>
2019-02-17 17:49:29 +0200

    added pytest for open port scanner
    Signed-off-by: Dmitry Grinberg <Dmitry.Grinberg@arm.com>

---
+++ mbl-core/tests/devices/open-ports-checker/test_open-ports-checker.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import mbl.open_ports_checker.open_ports_checker as opc
+import logging
+import os
+
+
+class TestOpenPortsScanner:
+    """ Open port scanner main class. """
+
+    def test_run_port_scanner(self):
+        """ Run open port scan test."""
+
+    info_level = logging.DEBUG
+
+    logging.basicConfig(
+        level=info_level,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+    logger = logging.getLogger("OpenPortsChecker")
+
+    white_list_file = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "mbl",
+        "open_ports_checker",
+        "white_list.json",
+    )
+    open_ports_checker = opc.OpenPortsChecker(white_list_file)
+    ret = open_ports_checker.run_check()
+    assert ret == opc.Status.SUCCESS
