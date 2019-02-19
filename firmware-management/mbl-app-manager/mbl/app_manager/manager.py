716d5575238c2f836ab8d86941037b4cccfa6e6c
Hugues Kamba <hugues.kamba@arm.com>
2019-02-19 11:54:04 +0000

    Refactor `mbl-app-manager` to make it easier to extend (#92)
    For IOTMBL-1607 Modify mbl-app-manager to allow specification of
    absolute path
    
    * Rename app_manager.py to manager.py.
    * Re-organise class in manager.py for flow clarity.
    * Add utils.py for utilities shared (such as logging and version).
    * Add parser.py for parsing ipk control data
    * Remove custom action to validate CLI input.
    * Add a combination of return codes and exceptions.
    * The app now has 0, 1, and 2 for return codes.
    * Sort import as recommended by PEP8.
    * Add useful doctsrings all around.
    * Make it easier to unit test and extend.
    * Add information to README.md

---
+++ firmware-management/mbl-app-manager/mbl/app_manager/manager.py
@@ -0,0 +1,161 @@
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Application Package installation."""
+
+import os
+import shutil
+import subprocess
+
+from .parser import parse_app_info, AppInfoParserError
+from .utils import log
+
+
+class AppManager(object):
+    """
+    Responsible for installing and removing application.
+
+    An application is a OPKG package with an `.ipk` extension.
+    """
+
+    def __init__(self):
+        """Create an app manager."""
+        # When opkg installs new package while "D" environment variable
+        # is defined, opkg will not call ldconfig in rootfs partition
+        os.environ["D"] = "1"
+
+    # ---------------------------- Public Methods -----------------------------
+
+    def get_app_name(self, app_pkg):
+        """Get the name of an application from the package meta data."""
+        log.debug("Getting app name from pkg '{}'".format(app_pkg))
+        app_name = ""
+        try:
+            app_name = parse_app_info(app_pkg)["Package"]
+        except AppInfoParserError as error:
+            msg = "Getting the app name from '{}' failed, error: {}".format(
+                app_pkg, str(error)
+            )
+            raise AppIdError(msg)
+        else:
+            log.info("Application name is '{}'".format(app_name))
+            return app_name
+
+    def install_app(self, app_pkg, app_path):
+        """Install an application.
+
+        The application is installed at a registered destination so the package
+        manager is able to install multiple version of the application, as
+        long as the registration path is different.
+        """
+        log.debug("installing package '{}' to '{}'".format(app_pkg, app_path))
+        # Command syntax:
+        # opkg --add-dest <app_name>:<app_path> install <app_pkg>
+        app_name = self.get_app_name(app_pkg)
+        dest = "{}:{}".format(app_name, app_path)
+        cmd = ["opkg", "--add-dest", dest, "install", app_pkg]
+        log.debug("Execute command: {}".format(" ".join(cmd)))
+        try:
+            subprocess.check_call(cmd)
+        except subprocess.CalledProcessError as error:
+            err_output = error.stdout.decode("utf-8")
+            msg = "Installing '{}' at '{}' failed, error: '{}'".format(
+                app_pkg, app_path, err_output
+            )
+            raise AppInstallError(msg)
+        else:
+            log.info(
+                "'{}' from '{}' package successfully installed at '{}'".format(
+                    app_name, app_pkg, app_path
+                )
+            )
+
+    def remove_app(self, app_name, app_path):
+        """Remove an application.
+
+        The application found at the registered destination is deleted.
+        """
+        log.debug("Removing app '{}' from '{}'".format(app_name, app_path))
+        if not os.path.isdir(app_path):
+            msg = "The application path '{}' does not exist".format(app_path)
+            raise AppPathInexistent(msg)
+        # Command syntax:
+        # opkg --add-dest <app_name>:<app_path> remove <app_name>
+        dest = "{}:{}".format(app_name, app_path)
+        cmd = ["opkg", "--add-dest", dest, "remove", app_name]
+        log.debug("Execute command: {}".format(" ".join(cmd)))
+        try:
+            subprocess.check_call(cmd)
+        except subprocess.CalledProcessError as error:
+            err_output = error.stdout.decode("utf-8")
+            msg = "Removing '{}' at '{}' failed, error: {}".format(
+                app_name, app_path, err_output
+            )
+            raise AppUninstallError(msg)
+        else:
+            log.info(
+                "'{}' removal from '{}' successful".format(app_name, app_path)
+            )
+            shutil.rmtree(app_path)
+
+    def force_install_app(self, app_pkg, app_path):
+        """
+        Force-install an application.
+
+        Remove a previous installation of the app at a specified location.
+        """
+        if os.path.isdir(app_path):
+            app_name = self.get_app_name(app_pkg)
+            self.remove_app(app_name, app_path)
+        self.install_app(app_pkg, app_path)
+
+    def list_installed_apps(self, apps_path):
+        """List all installed applications.
+
+        Each application is supposed to have a directory within `apps_path`.
+        Each application directory is named after the installed application.
+        """
+        log.debug("List of installed applications:\n")
+        subdirs = [
+            name
+            for name in os.listdir(apps_path)
+            if os.path.isdir(os.path.join(apps_path, name))
+        ]
+        for subdir in subdirs:
+            # Command syntax:
+            # opkg --add-dest <app_name>:<apps_path>/<app_name> list-installed
+            app_name = subdir
+            dest = "{}:{}".format(app_name, os.path.join(apps_path, app_name))
+            cmd = ["opkg", "--add-dest", dest, "list-installed"]
+            try:
+                subprocess.check_call(cmd)
+            except subprocess.CalledProcessError as error:
+                err_output = error.stdout.decode("utf-8")
+                msg = (
+                    "Listing installed apps at '{}' failed,"
+                    " error: '{}'".format(
+                        os.path.join(apps_path, app_name), err_output
+                    )
+                )
+                raise AppListingError(msg)
+
+
+class AppIdError(Exception):
+    """An exception for a missing application name."""
+
+
+class AppInstallError(Exception):
+    """An exception for an app installation failure."""
+
+
+class AppUninstallError(Exception):
+    """An exception for an app uninstallation failure."""
+
+
+class AppPathInexistent(Exception):
+    """An exception raised when an application path is inexistent."""
+
+
+class AppListingError(Exception):
+    """An exception for a failure to list installed apps."""
---
+++ firmware-management/mbl-app-manager/mbl/app_manager/utils.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+"""Contains utilities."""
+
+import logging
+
+log = logging.getLogger(__name__)
+
+HIGH_VERBOSITY_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+LOW_VERBOSITY_FORMAT = "%(message)s"
+
+
+def set_log_verbosity(increase_verbosity):
+    """Set the verbosity of the log output."""
+    log_level = logging.DEBUG if increase_verbosity else logging.INFO
+    log_format = (
+        HIGH_VERBOSITY_FORMAT if increase_verbosity else LOW_VERBOSITY_FORMAT
+    )
+
+    log.setLevel(log_level)
+    logging.basicConfig(level=log_level, format=log_format)
