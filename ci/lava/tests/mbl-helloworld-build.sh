ea743a0a0b75abf1e964f342f03d3182198f9dfa
Pete Dyer <pete.dyer@arm.com>
2019-02-22 16:16:57 +0000

    Automate hello world sample app
    Added lava test definitions for building the hello world sample app,
    transferring to the DUT using the mbl-cli, installing the app and
    collecting the results.
    
    install_docker.yaml installs the necessary packages into an Ubuntu lxc
    container to build the sample app
    
    mbl-helloworld-build.yaml calls mbl-helloworld-build.sh inside an lxc
    container to build the sample application
    
    mbl-helloworld-run.yaml calls mbl-helloworld-run.sh which perform the steps
    of interfacing with the DUT using the mbl-cli. This in turn uses
    check_container.py  to ensure the user sample app is ruggins and
    transitions to stopped within a 30 second window.

---
+++ ci/lava/tests/mbl-helloworld-build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+cd tutorials/helloworld || exit
+
+docker build -t linux-armv7:latest ./cc-env/
+
+docker run --rm linux-armv7 > ./build-armv7
+
+chmod +x ./build-armv7
+
+./build-armv7 make release
+
+./build-armv7 make debug
+
+cd debug/ipk/ || exit
+
+tar -cvf user-sample-app-package_1.0_armv7vet2hf-neon.ipk.tar user-sample-app-package_1.0_armv7vet2hf-neon.ipk
+cp user-sample-app-package_1.0_armv7vet2hf-neon.ipk.tar /home/ubuntu/user-sample-app-package_1.0_armv7vet2hf-neon.ipk.tar
+
