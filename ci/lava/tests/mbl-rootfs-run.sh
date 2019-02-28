0dc854351eb96b25068b4a84d2c45225dad7fb1f
Pete Dyer <pete.dyer@arm.com>
2019-02-28 16:01:29 +0000

    Baby steps

--- ci/lava/tests/mbl-rootfs-run.sh
+++ ci/lava/tests/mbl-rootfs-run.sh
@@ -5,7 +5,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Find and select the device to talk to
-apt install wget
 
 mbl-cli list > device_list
 dut_address=$(grep "mbed-linux-os" device_list | cut -d":" -f3-)
@@ -22,20 +21,14 @@ else
 
     mbl_command="mbl-cli -a $dut_address"
 
-    sleep 6000
     wget http://artifactory-proxy.mbed-linux.arm.com/artifactory/isg-mbed-linux/mbed-linux/mbl-master/mbl-master.1148/machine/imx7s-warp-mbl/images/mbl-image-development/images/mbl-image-development-imx7s-warp-mbl.tar.xz/mbl-image-development-imx7s-warp-mbl.tar.xz 
-    sleep 6000
 
     # Now copy the package and ptyhon checker script to the DUT
     $mbl_command put mbl-image-development-imx7s-warp-mbl.tar.xz /home/root
-    sleep 6000
 
     # Now install the package - this should cause it to run
-    $mbl_command shell "mbl-app-firmware-manager -i /home/root/mbl-image-development-imx7s-warp-mbl.tar.xz"
+    $mbl_command shell "mbl-firmware-update-manager-app-firmware-manager -i /home/root/mbl-image-development-imx7s-warp-mbl.tar.xz -s -v"
 
-    sleep 6000
-
-    $mbl_command list
 
 fi
 
