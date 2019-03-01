5c5ffed27654e8734d49bc7990c228cf2f04f717
Hugues Kamba <hugues.kamba@arm.com>
2019-03-01 08:18:53 +0000

    TBS: Add information to `human_sort` utility function docstring

--- application-framework/mbl-app-lifecycle-manager/mbl/app_lifecycle_manager/utils.py
+++ application-framework/mbl-app-lifecycle-manager/mbl/app_lifecycle_manager/utils.py
@@ -24,7 +24,12 @@ def set_log_verbosity(increase_verbosity):
 
 
 def human_sort(l):
-    """Sort the given list in the way that humans expect."""
+    """Sort the given list in ascending order the way that humans expect.
+
+    i.e For a list l containing `["5", "24", "10"]`, the function sorts it as
+    `["5", "10", "24"]` instead of `["10", "24", "5"]` as would Python List
+    sort() method.
+    """
     # key to use for the sort
     def convert(text):
         return int(text) if text.isdigit() else text
