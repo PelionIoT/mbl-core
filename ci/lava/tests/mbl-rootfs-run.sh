017367a1e0ab3a13789aea0290fbd33451233626
Pete Dyer <pete.dyer@arm.com>
2019-03-01 09:31:51 +0000

    look for linux in devie name

--- ci/lava/tests/mbl-rootfs-run.sh
+++ ci/lava/tests/mbl-rootfs-run.sh
@@ -7,9 +7,17 @@
 # Find and select the device to talk to
 
 mbl-cli list > device_list
+cat device_list
+
 dut_address=$(grep "mbed-linux-os" device_list | cut -d":" -f3-)
 
-cat device_list
+if [ -z "$dut_address" ]
+then
+    echo "ERROR - mbl-cli failed to find MBL device"
+    echo "ERROR - mbl-cli retry with linux"
+    dut_address=$(grep "linux" device_list | cut -d":" -f3-)
+fi
+
 
 rm device_list
 
