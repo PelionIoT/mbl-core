892007464bb4de8fb7d87f542ea715432e335498
Pete Dyer <pete.dyer@arm.com>
2019-03-07 10:57:04 +0000

    Changed rootfs image reference

--- ci/lava/tests/mbl-rootfs-run.sh
+++ ci/lava/tests/mbl-rootfs-run.sh
@@ -4,11 +4,21 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Find and select the device to talk to
+
+# This script must have 2 parameters passed, the test stage (UPDATE or
+# POST_CHECK) and the image_url of the wic file.
+#
+# Set the local variables:
+# $test_stage contaons the stage
+# $rootfs_image is the image name with the "wic.gz" replaced by "tar.xz"
+#
 
 test_stage=$1
-image_name=${$2//wic.gz/tar.xz}
-echo $image_name
+
+# Perform global substitution
+rootfs_image=${2//wic.gz/tar.xz}
+
+# Find and select the device to talk to
 
 mbl-cli list > device_list
 cat device_list
@@ -33,6 +43,7 @@ else
 
     mbl_command="mbl-cli -a $dut_address"
 
+    # Work out current active partition. 
     active_partition=`$mbl_command shell 'lsblk --noheadings --output "MOUNTPOINT,LABEL"' | awk '$1=="/" {print $2}'`
 
     echo -n "Active Partition: "
@@ -41,7 +52,8 @@ else
 
     if [ $test_stage = "UPDATE" ]
     then
-        # At the start rootfs1 should be the active partition.
+        # At the start rootfs1 should be the active partition, since the board
+        # should be cleanly flashed.
         if [ $active_partition = "rootfs1" ]
         then
             echo "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=rootfs1_selected RESULT=pass>"
@@ -49,13 +61,16 @@ else
             echo "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=rootfs1_selected RESULT=fail>"
         fi
 
+        # Update the hostname of the DUT to contain "lava-". This is so the
+        # test stage after the reboot will expect a different prompt.
         $mbl_command shell 'echo "lava-"`hostname` > /config/user/hostname'
 
+        # Install wget
         apt-get update
         apt-get install -q -q --yes wget
 
         # Get the root filesystem image from the server.
-        wget $image_name
+        wget $rootfs_image
 
         # Tar it up in the expected manner
         tar -cf payload.tar mbl-image-development-imx7s-warp-mbl.tar.xz '--transform=s/.*/rootfs.tar.xz/'
@@ -69,9 +84,11 @@ else
         # Now reboot the board and get the result of the reboot command
         $mbl_command shell 'su -l -c "reboot || echo $?"'
 
-        # Sleep to allow the reboot to happen
-        sleep 60
-    else
+        # Sleep to allow the reboot to happen. This is nasty but is long enough
+        # for the DUT to shut down but not long enough for it to fully restart.
+        sleep 40
+
+    else # The POST_CHECK
         # At the end rootfs2 should be the active partition.
         if [ $active_partition = "rootfs2" ]
         then
