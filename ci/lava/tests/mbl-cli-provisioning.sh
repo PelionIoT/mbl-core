9934d408031f5e81ee1472a23b4b59878419e277
Pete Dyer <pete.dyer@arm.com>
2019-03-19 14:32:14 +0000

    Debug

--- ci/lava/tests/mbl-cli-provisioning.sh
+++ ci/lava/tests/mbl-cli-provisioning.sh
@@ -30,22 +30,46 @@ else
 
 set -x
 
-    pip install --upgrade pip
+    # Install the manifest-tool. This needs a newer version of pip, so get that
+    # as well.
+    pip3 -qqq install --upgrade pip
+    pip3 -qqq install manifest-tool
 
-    pip3 install manifest-tool
-
+    # Work out a unique certificate id - the first part of the path is the lava 
+    # job number.
     certificate="$(pwd | cut -d"/" -f2)"
+    api_key=ak_1MDE1ZTcxMTg4OGM4MDI0MjBhMDEwZDA1MDAwMDAwMDA0169725e06d512dc3bb0a403000000006nOrZ6DW1Ms5Gx5W3vU5O1zsFIyrrH
 
+    # Run the manifest tool
     mkdir /tmp/update-resources
     cd /tmp/update-resources
-
     manifest-tool init -q -d arm.com -m dev-device
 
-    mbl-cli save-api-key ak_1MDE1ZTcxMTg4OGM4MDI0MjBhMDEwZDA1MDAwMDAwMDA0169725e06d512dc3bb0a403000000006nOrZ6DW1Ms5Gx5W3vU5O1zsFIyrrHAv
+    # Deal with API keys.
 
-    cat ~/.mbl-store/user/config.json
+    print "Try to save the api key.\n"
+    mbl-cli save-api-key $api_key
 
-    mbl-cli save-api-key ahdskjhdakhkjas
+    print "Check to see the key is installed correctly\n"
+    key_found=grep -c $key ~/.mbl-store/user/config.json
+
+    if [ $key_found eq 1 ]
+    then
+        print "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=add-valid-key RESULT=pass>\n"
+    else
+        print "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=add-valid-key RESULT=fail>\n"
+    fi
+
+
+    mbl-cli save-api-key invalid_key >& /tmp/invalid_key
+    invalid_rejected_ok=grep -c "API key not recognised by Pelion Device Management." ~/.mbl-store/user/config.json
+
+    if [ $invalid_rejected_ok eq 1 ]
+    then
+        print "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=reject-invalid-key RESULT=pass>\n"
+    else
+        print "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=reject-invalid-key RESULT=fail>\n"
+    fi
 
     $mbl_command get-pelion-status
 
