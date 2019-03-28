b6e99db57dda1ede08b011f2aa632b01481f900d
MaximAxelrod <maxim.axelrod@arm.com>
2019-03-28 18:31:18 +0200

    Modify hello_pelion_connect app to use Alpine ARM Linux image
    Before this change, hello_pelion_connect mounted the existing device rootfs folders to the OCI container. After this change, hello_pelion_connect runs in the OCI container based on the Alpine ARM Linux image, on which the hello_pelion_connect application was installed.
    
    Jira: IOTMBL-1868

---
+++ tutorials/hello_pelion_connect/scripts/dockerize-hello-pelion-connect-app.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright (c) 2018 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+# Creates hello_pelion_connect application package.
+#
+# To clean previously created artifacts call script with "clean" argument.
+# FIXME ADD DESCRIPTION
+
+#abort on error
+set -e
+
+# Current directory absolute path
+SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
+
+# clean
+if [ "$1" = "clean" ]; then
+
+    echo "*** Removing an existing artifacts"
+    rm -rf "${SCRIPT_DIR}/../release"
+    exit 0
+fi
+
+
+# Build the hello-pelion-connect docker image
+echo "Building the hello-pelion-connect docker image..."
+docker build -t hello-pelion-connect "$SCRIPT_DIR/.."
+echo "Exporting the docker image into a tar archive..."
+mkdir "$SCRIPT_DIR/../release"
+docker export -o "$SCRIPT_DIR/../release/hello-pelion-connect-docker-image.tar" "$(docker create hello-pelion-connect)"
+echo "Extracting content from docker container..."
+mkdir -p "$SCRIPT_DIR/../release/runtime-bundle-filesystem"
+# Extract the filesystem with elevated permission for the operation to succeed
+sudo tar -C "$SCRIPT_DIR/../release/runtime-bundle-filesystem" -xf "$SCRIPT_DIR/../release/hello-pelion-connect-docker-image.tar"
+sudo chown -R "$(id -un)":"$(id -gn)" "release/runtime-bundle-filesystem"
+
+echo "****************************** Creating container:*****************************************"
+docker build "${SCRIPT_DIR}/../cc-env/" -t hello_pelion_connect_builder
+
+echo "****************************** Running container and creating package inside the container:"
+docker run --rm -e USER_NAME="$(whoami)" -e USER_ID="$UID" \
+-v "$(realpath "${SCRIPT_DIR}/../")":/hello_pelion_connect \
+-v /etc/localtime:/etc/localtime:ro \
+-v /etc/timezone:/etc/timezone:ro \
+-it hello_pelion_connect_builder
