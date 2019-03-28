8bdcecf65af5babb34d3c79863415e18b3266a74
Hugues Kamba <hugues.kamba@arm.com>
2019-03-28 10:28:59 +0000

    Use the same log format regardless of the verbosity for MBL python packages

--- firmware-management/mbl-app-update-manager/mbl/app_update_manager/utils.py
+++ firmware-management/mbl-app-update-manager/mbl/app_update_manager/utils.py
@@ -11,19 +11,15 @@ import mbl.app_lifecycle_manager.utils as alu
 
 log = logging.getLogger(__name__)
 
-HIGH_VERBOSITY_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-LOW_VERBOSITY_FORMAT = "%(message)s"
+VERBOSITY_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 
 def set_log_verbosity(increase_verbosity):
     """Set the verbosity of the log output."""
     log_level = logging.DEBUG if increase_verbosity else logging.INFO
-    log_format = (
-        HIGH_VERBOSITY_FORMAT if increase_verbosity else LOW_VERBOSITY_FORMAT
-    )
 
     log.setLevel(log_level)
-    logging.basicConfig(level=log_level, format=log_format)
+    logging.basicConfig(level=log_level, format=VERBOSITY_FORMAT)
     # Syncronize the log level of all MBL dependencies
     alu.set_log_verbosity(increase_verbosity)
     apu.set_log_verbosity(increase_verbosity)
