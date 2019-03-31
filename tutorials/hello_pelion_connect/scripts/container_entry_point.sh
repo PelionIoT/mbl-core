fa2b5199eedd782971840b52b60412da7b8d6436
MaximAxelrod <maxim.axelrod@arm.com>
2019-03-31 11:59:14 +0300

    PR fixes

--- tutorials/hello_pelion_connect/scripts/container_entry_point.sh
+++ tutorials/hello_pelion_connect/scripts/container_entry_point.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2018 Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
