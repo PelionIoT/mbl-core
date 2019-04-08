e1a8ca5c7493c9ee8edfe9120c01255eb68fdd80
Pete Dyer <pete.dyer@arm.com>
2019-04-08 14:05:05 +0100

    Change to the manifest-tool line

--- ci/lava/tests/mbl-cli-pelion-app-update.sh
+++ ci/lava/tests/mbl-cli-pelion-app-update.sh
@@ -40,7 +40,7 @@ else
 
     $mbl_command get /var/log/mbl-cloud-client.log /tmp/mbl-cloud-client.log
 
-    device_id=$(grep -i "device id"  /tmp/mbl-cloud-client.log | tail -1 | cut -d":" -f5)
+    device_id=$(grep -i "device id"  /tmp/mbl-cloud-client.log | tail -1 | cut -d":" -f5 | cut -c2-)
 
     if [ -z "$device_id" ]
     then
