213f6da6595dec1e6fd1073eb8283365d65cc2cb
Pete Dyer <pete.dyer@arm.com>
2019-04-30 14:03:07 +0100

    testing

--- ci/lava/tests/mbl-cli-provisioning.sh
+++ ci/lava/tests/mbl-cli-provisioning.sh
@@ -6,23 +6,31 @@
 
 # Run the Pelion provisioning tests and confirm they are functioning correctly
 
-# The first parameter passed in is the path to the Python virtual environment 
-# that contains the mbl-cli.
-# The optional second parameter is the pattern to identify the 
-# target board, otherwise find something with "mbed-linux-os" in it.
+# Parse the inputs
+#
+# The Python virtual environment will be cactivated if provided.
+# The device under test can also be specified.
 
-cli_path=$1
+# Default to any device
+pattern="mbed-linux-os"
 
-if [ -z "$2" ]
-then
-    pattern="mbed-linux-os"
-else
-    pattern=$1
-fi
+while [ "$1" != "" ]; do
+    case $1 in
+        -v | --venv-path )      shift
+                                source  $1/activate
+                                ;;
+        -d | --dut )            shift
+                                pattern=$1
+                                ;;
+        * )                     echo "Invalid Parameter"
+                                exit 1
+    esac
+    shift
+done
 
 # Find the address of the first device found by the mbl-cli containing the
 # pattern
-$cli_path/mbl-cli list > device_list
+mbl-cli list > device_list
 dut_address=$(grep "$pattern" device_list | head -1 | cut -d":" -f3-)
 
 # list the devices for debug
@@ -31,7 +39,7 @@ cat device_list
 # Tidy up
 rm device_list
 
-mbl_command="$cli_path/mbl-cli -a $dut_address"
+mbl_command="mbl-cli -a $dut_address"
 
 
 # Only proceed if a device has been found
@@ -45,7 +53,7 @@ else
     overall_result="pass"
 
     # Install the manifest-tool.
-    $cli_path/pip3 -qqq install manifest-tool
+    pip3 -qqq install manifest-tool
 
     # Work out a unique certificate id - the first part of the path is the lava 
     # job number.
@@ -55,7 +63,7 @@ else
     mkdir /tmp/update-resources
     cd /tmp/update-resources || exit
 
-    if $cli_path/manifest-tool init -q -d arm.com -m dev-device >& /tmp/manifest-init
+    if manifest-tool init -q -d arm.com -m dev-device >& /tmp/manifest-init
     then
         printf "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=manifest-init RESULT=pass>\n"
     else
@@ -66,7 +74,7 @@ else
 
 
     # Attempt to save an invalid key and check the result is as expected.
-    $cli_path/mbl-cli save-api-key invalid_key >& /tmp/invalid_key
+    mbl-cli save-api-key invalid_key >& /tmp/invalid_key
     invalid_rejected_ok=$(grep -c "API key not recognised by Pelion Device Management." /tmp/invalid_key)
 
     if [ "$invalid_rejected_ok" -eq 1 ]
