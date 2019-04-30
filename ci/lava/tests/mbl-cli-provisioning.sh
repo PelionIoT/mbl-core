63c7b4f7aadcbc846998a15004156de111276b40
Pete Dyer <pete.dyer@arm.com>
2019-04-30 09:24:02 +0100

    Use the Python virtual environment.
    Changed to use the mbl cli directly out of the virtual environment.

--- ci/lava/tests/mbl-cli-provisioning.sh
+++ ci/lava/tests/mbl-cli-provisioning.sh
@@ -6,9 +6,14 @@
 
 # Run the Pelion provisioning tests and confirm they are functioning correctly
 
-# If a parameter is passed in then assume it is a pattern to identify the
+# The first parameter passed in is the path to the Python virtual environment 
+# that contains the mbl-cli.
+# The optional second parameter is the pattern to identify the 
 # target board, otherwise find something with "mbed-linux-os" in it.
-if [ -z "$1" ]
+
+cli-path=$1
+
+if [ -z "$2" ]
 then
     pattern="mbed-linux-os"
 else
@@ -17,7 +22,7 @@ fi
 
 # Find the address of the first device found by the mbl-cli containing the
 # pattern
-mbl-cli list > device_list
+$cli-path/mbl-cli list > device_list
 dut_address=$(grep "$pattern" device_list | head -1 | cut -d":" -f3-)
 
 # list the devices for debug
@@ -26,7 +31,7 @@ cat device_list
 # Tidy up
 rm device_list
 
-mbl_command="mbl-cli -a $dut_address"
+mbl_command="$cli-path/mbl-cli -a $dut_address"
 
 
 # Only proceed if a device has been found
@@ -40,7 +45,7 @@ else
     overall_result="pass"
 
     # Install the manifest-tool.
-    pip3 -qqq install manifest-tool
+    $cli-path/pip3 -qqq install manifest-tool
 
     # Work out a unique certificate id - the first part of the path is the lava 
     # job number.
@@ -50,7 +55,7 @@ else
     mkdir /tmp/update-resources
     cd /tmp/update-resources || exit
 
-    if manifest-tool init -q -d arm.com -m dev-device >& /tmp/manifest-init
+    if $cli-path/manifest-tool init -q -d arm.com -m dev-device >& /tmp/manifest-init
     then
         printf "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID=manifest-init RESULT=pass>\n"
     else
@@ -61,7 +66,7 @@ else
 
 
     # Attempt to save an invalid key and check the result is as expected.
-    mbl-cli save-api-key invalid_key >& /tmp/invalid_key
+    $cli-path/mbl-cli save-api-key invalid_key >& /tmp/invalid_key
     invalid_rejected_ok=$(grep -c "API key not recognised by Pelion Device Management." /tmp/invalid_key)
 
     if [ "$invalid_rejected_ok" -eq 1 ]
