852da8a0f1763dd50fd60423a593a8ec53e121a3
Pete Dyer <pete.dyer@arm.com>
2019-09-05 15:06:53 +0100

    Refactor to remove mbl-scratch dependency. (#207)
    * Create tooling to remove dependency on mbl-scratch.
    
    Added files to enable the use of pytest to build test images and run
    tests on either LXC or devie under test.
    
    Added conftest to output LAVA compatible strings instead of scraping the
    XML output in seperate scripts.
    
    Added fixtures into conftest to include helpers common to every test,
    
    Added pytest files to build test applications instead of using the
    pre-built images from mbl-scratch.
    
    Changed the core component tests into a pytest that is run inside the
    LXC container, thus removign the need to inject the tests directly into
    the rootfs on the device under test.
    
    * Embed mbl-cloud-client restart in provisioning script
    
    * Enable wifi on imx6ul-pico-mbl
    
    * Create tooling to remove dependency on mbl-scratch.
    
    Added yaml for creating python virtual environment and installing pytest
    and junitparser into the LXC container.
    
    Converted systemd test from bash to pytest. Removed mnt.flags test as
    that partition is no longer used.

---
+++ ci/lava/dependencies/test-install-pytest-to-target.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Script to install test dependencies onto the DUT.
+
+The test is run using pytest and communicates with the DUT using the
+mbl-cli which is previously installed as part of the wider LAVA test.
+
+This script also assumes they pytest has been downloaded
+into host_download_dir, also as part of the wider LAVA test.
+As the LAVA test is using pytest to run this script then reasonable assumption.
+
+"""
+import pytest
+import re
+
+
+class Test_Create_Pytest_Environment_On_DUT:
+    """Create python test environment on a device under test."""
+
+    def test_pytest_to_target(
+        self, execute_helper, dut_addr, host_download_dir, dut_download_dir
+    ):
+        """Copy pytest onto the DUT."""
+        copy_success = False
+
+        returnCode, put_output, error = execute_helper.send_mbl_cli_command(
+            ["put", "-r", host_download_dir, dut_download_dir], dut_addr
+        )
+
+        pattern = re.compile(r"Transfer completed")
+
+        for line in put_output.splitlines():
+            match = pattern.match(line)
+            if match:
+                copy_success = True
+                break
+        assert copy_success
+
+    def test_create_python_virtual_environment_on_target(
+        self, execute_helper, dut_addr, venv
+    ):
+        """Create the python virtual environment on the DUT."""
+        returnCode, output, error = execute_helper.send_mbl_cli_command(
+            [
+                "shell",
+                "python3 -m venv {} --system-site-packages".format(venv),
+            ],
+            dut_addr,
+        )
+
+        assert returnCode == 0
+
+    def test_install_pytest_on_target(
+        self, execute_helper, dut_addr, dut_download_dir, venv
+    ):
+        """Install pytest into the python virtual environment on the DUT."""
+        returnCode, output, error = execute_helper.send_mbl_cli_command(
+            [
+                "shell",
+                "{}/bin/pip3 "
+                "install "
+                "--no-index "
+                "--find-links="
+                "{} pytest".format(venv, dut_download_dir),
+            ],
+            dut_addr,
+        )
+
+        assert returnCode == 0
---
+++ ci/lava/tests/test-core-component.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Script to install and run core component tests on the DUT.
+
+This pytest script consists of a test_setup function that installs the
+necessary pre-built images and builds and installs images for firmware
+testing onto the DUT. It also copies the entire mbl-core clone onto the DUT.
+
+The test_component function runs pytest on the DUT via the mbl-cli on the
+mbl-core copy.
+
+"""
+import pytest
+import re
+import subprocess
+
+# The test expects applications to be copied into the "/home/app" directory on
+# the device
+dut_app_home = "/home"
+
+
+class Test_Core_Component_DUT:
+    """Class to encapsulate the testing of core components on a DUT."""
+
+    def test_setup(
+        self, dut_addr, execute_helper, host_tutorials_dir, dut_tutorials_dir
+    ):
+        """Copy the test specific parts, generating items as required."""
+        execute_helper.send_mbl_cli_command(
+            [
+                "put",
+                "{}/user-sample-app-package_1.0_any.ipk".format(
+                    host_tutorials_dir
+                ),
+                "{}/app-lifecycle-manager-test-package_1.0_any.ipk".format(
+                    dut_tutorials_dir
+                ),
+            ],
+            dut_addr,
+        )
+
+        execute_helper.send_mbl_cli_command(
+            [
+                "put",
+                "{}/mbl-multi-apps-update-package-all-good.tar".format(
+                    host_tutorials_dir
+                ),
+                dut_tutorials_dir,
+            ],
+            dut_addr,
+        )
+
+        execute_helper.send_mbl_cli_command(
+            [
+                "put",
+                "{}/mbl-multi-apps-update-package-one-fail-run.tar".format(
+                    host_tutorials_dir
+                ),
+                dut_tutorials_dir,
+            ],
+            dut_addr,
+        )
+
+        execute_helper.send_mbl_cli_command(
+            [
+                "put",
+                "{}/mbl-multi-apps-update-package-one-fail-install.tar".format(
+                    host_tutorials_dir
+                ),
+                dut_tutorials_dir,
+            ],
+            dut_addr,
+        )
+
+        # Build the test images
+        execute_helper.execute_command(
+            [
+                "python3",
+                "./firmware-management/mbl-app-manager/tests/native/"
+                "test_case_generator_mbl-app-manager.py",
+                "-o",
+                "{}/app".format(host_tutorials_dir),
+                "-v",
+            ]
+        )
+
+        # Copy the test images to the DUT
+        execute_helper.send_mbl_cli_command(
+            ["put", "-r", "{}/app".format(host_tutorials_dir), dut_app_home],
+            dut_addr,
+        )
+
+        # Copy the common parts to the DUT
+        execute_helper.send_mbl_cli_command(
+            ["put", "-r", "./", "{}/mbl-core".format(dut_tutorials_dir)],
+            dut_addr,
+        )
+
+        # Copy the conftest to the DUT
+        execute_helper.send_mbl_cli_command(
+            [
+                "put",
+                "./ci/lava/conftest.py",
+                "{}/mbl-core".format(dut_tutorials_dir),
+            ],
+            dut_addr,
+        )
+
+        assert True
+
+    def test_component(
+        self, dut_addr, execute_helper, venv, dut_tutorials_dir
+    ):
+        """Perform the test on the DUT via the mbl-cli."""
+        return_code, output, error = execute_helper.send_mbl_cli_command(
+            [
+                "shell",
+                "{}/bin/pytest "
+                "--verbose "
+                "--ignore={}/mbl-core/ci --color=no "
+                "{}/mbl-core".format(
+                    venv, dut_tutorials_dir, dut_tutorials_dir
+                ),
+            ],
+            dut_addr,
+        )
+        # print the output to get any LAVA format strings displayed and
+        # processed by LAVA.
+        print(output)
+
+        assert True
