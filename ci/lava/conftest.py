de1b55df28e54e093cbe2374c0bcf1120fe6add6
Rob Walton <rob.walton@arm.com>
2019-11-08 10:01:30 +0000

    ci:lava:tests: Add image update test.
    Add a test to validate image updates using the test info file
    generated by our create-update-payload script.
    
    Also add a helpers.py module and move all test helper functions
    there. Leave fixtures in conftest.py

--- ci/lava/conftest.py
+++ ci/lava/conftest.py
@@ -7,11 +7,75 @@
 
 import os
 import pytest
-import re
-import subprocess
-import sys
-import tempfile
-import urllib.request
+
+from helpers import download_from_url, get_dut_address, ExecuteHelper
+
+
+def pytest_addoption(parser):
+    """Add option parsing to pytest."""
+    # Common parameters
+    parser.addoption("--venv", action="store", default="/tmp/venv")
+    parser.addoption("--debug-output", action="store_true")
+    parser.addoption("--device", action="store", default="none")
+    parser.addoption("--dut", action="store", default="auto")
+    parser.addoption(
+        "--dut-download-dir", action="store", default="/tmp/pytest"
+    )
+    parser.addoption(
+        "--host-download-dir", action="store", default="/tmp/pytest"
+    )
+
+    # Tutorial tests parameters
+    parser.addoption("--dut-tutorials-dir", action="store", default="/scratch")
+    parser.addoption(
+        "--host-tutorials-dir", action="store", default="/tmp/tutorials"
+    )
+    parser.addoption("--payload-version", action="store", default="1")
+
+    # local.conf tests parameters
+    parser.addoption("--local-conf-file", action="store", default="")
+
+    # Boot component update test parameters
+    parser.addoption(
+        "--update-component-name",
+        action="store",
+        choices=["bootloader1", "bootloader2", "kernel", "rootfs"],
+    )
+    parser.addoption("--update-payload-url", action="store")
+    parser.addoption("--update-payload-testinfo-url", action="store")
+    parser.addoption(
+        "--update-method",
+        action="store",
+        default="mbl-cli",
+        choices=["pelion", "mbl-cli"],
+    )
+
+
+def pytest_configure(config):
+    """Register custom markers."""
+    config.addinivalue_line(
+        "markers",
+        "pelion: mark a test to be run when the update method is pelion",
+    )
+    config.addinivalue_line(
+        "markers",
+        "mbl_cli: mark a test to be run when the update method is mbl-cli",
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    """Select tests depending the update-method passed as argument."""
+    update_method = config.getoption("--update-method")
+    skip = pytest.mark.skip(
+        reason="update-method {} selected".format(update_method)
+    )
+    for item in items:
+        markers = set(m.name for m in item.iter_markers())
+        # We skip tests which don't match the update_method
+        # mbl-cli is replaced with mbl_cli
+        if markers and update_method.replace("-", "_") not in markers:
+            print(item)
+            item.add_marker(skip)
 
 
 def pytest_report_teststatus(report):
@@ -22,70 +86,29 @@ def pytest_report_teststatus(report):
                 report.nodeid.replace(" ", "_")
             )
             return report.outcome, "*", lava
-
     if report.failed:
         lava = "<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={} RESULT=fail>".format(
             report.nodeid.replace(" ", "_")
         )
         return report.outcome, "*", lava
 
-
-def pytest_addoption(parser):
-    """Add option parsing to pytest."""
-    parser.addoption("--debug-output", action="store_true")
-    parser.addoption("--device", action="store", default="none")
-    parser.addoption("--dut", action="store", default="auto")
-    parser.addoption(
-        "--dut-download-dir", action="store", default="/tmp/pytest"
-    )
-    parser.addoption("--dut-tutorials-dir", action="store", default="/scratch")
-    parser.addoption(
-        "--host-download-dir", action="store", default="/tmp/pytest"
-    )
-    parser.addoption(
-        "--host-tutorials-dir", action="store", default="/tmp/tutorials"
-    )
-    parser.addoption("--local-conf-file", action="store", default="")
-    parser.addoption("--payload-version", action="store", default="1")
-    parser.addoption("--venv", action="store", default="/tmp/venv")
+def pytest_generate_tests(metafunc):
+    """Select tests that have the update_component_name specified as marker."""
+    # This is called for every test. Only get/set command line arguments
+    # if the argument is specified in the list of test "fixturenames".
+    option_value = metafunc.config.option.update_component_name
+    if (
+        "update_component_name" in metafunc.fixturenames
+        and option_value is not None
+    ):
+        metafunc.parametrize("update_component_name", [option_value])
 
 
-def _download_from_url(url):
-
-    filename = None
-
-    if url:
-
-        filename = os.path.join(tempfile.mkdtemp(), os.path.basename(url))
-
-        try:
-            urllib.request.urlretrieve(url, filename)
-        except Exception as inst:
-            print("\n\nError {}\n\n".format(type(inst)))
-            print(
-                "urlretrieve failed. Trying alternative method by adding "
-                "mapping between 192.168.130.43 and "
-                "artifactory-proxy.mbed-linux.arm.com into /etc/hosts."
-            )
-
-            try:
-                f = open("/etc/hosts", "a")
-                f.write(
-                    "192.168.130.43  artifactory-proxy.mbed-linux.arm.com\n"
-                )
-            except Exception as inst:
-                print("\n\nError {}\n\n".format(type(inst)))
-                print("Could not update /etc/hosts. Perhaps run as root?")
-
-            # re-try the urlretrieve.
-            try:
-                urllib.request.urlretrieve(url, filename)
-            except Exception as inst:
-                print("\n\nError {}\n\n".format(type(inst)))
-                print("\n\nAlternative method also failed.\n\n")
-                filename = None
-
-    return filename
+# common fixtures
+@pytest.fixture
+def venv(request):
+    """Fixture for --venv."""
+    return request.config.getoption("--venv")
 
 
 @pytest.fixture
@@ -113,15 +136,16 @@ def dut_download_dir(request):
 
 
 @pytest.fixture
-def dut_tutorials_dir(request):
-    """Fixture for --dut-tutorials-dir."""
-    return request.config.getoption("--dut-tutorials-dir")
-
-
-@pytest.fixture
 def host_download_dir(request):
     """Fixture for --host-download-dir."""
     return request.config.getoption("--host-download-dir")
+
+
+# Tutorials fixtures
+@pytest.fixture
+def dut_tutorials_dir(request):
+    """Fixture for --dut-tutorials-dir."""
+    return request.config.getoption("--dut-tutorials-dir")
 
 
 @pytest.fixture
@@ -130,6 +154,13 @@ def host_tutorials_dir(request):
     return request.config.getoption("--host-tutorials-dir")
 
 
+@pytest.fixture
+def payload_version(request):
+    """Fixture for --payload-version."""
+    return request.config.getoption("--payload-version")
+
+
+# local.conf fixtures
 @pytest.fixture
 def local_conf_file(request):
     """
@@ -141,96 +172,45 @@ def local_conf_file(request):
     Else return None
     """
     if request.config.getoption("--local-conf-file").startswith("http"):
-        return _download_from_url(
-            request.config.getoption("--local-conf-file")
-        )
+        return download_from_url(request.config.getoption("--local-conf-file"))
     elif os.path.exists(request.config.getoption("--local-conf-file")):
         return request.config.getoption("--local-conf-file")
     else:
         return None
 
 
+# Component update fixtures
 @pytest.fixture
-def payload_version(request):
-    """Fixture for --payload-version."""
-    return request.config.getoption("--payload-version")
+def update_component_name(request):
+    """Fixture for --update-component-name."""
+    return request.config.getoption("--update-component-name")
 
 
 @pytest.fixture
-def venv(request):
-    """Fixture for --venv."""
-    return request.config.getoption("--venv")
+def update_payload(request):
+    """Fixture for --update-payload-url."""
+    return download_from_url(request.config.getoption("--update-payload-url"))
 
 
-class ExecuteHelper:
-    """Class to provide a wrapper for executing commands as a subprocess."""
+@pytest.fixture
+def update_payload_testinfo(request):
+    """
+    Fixture for --update-payload-testinfo-url.
 
-    debug = False
-
-    def __init__(self, request):
-        """Initialise the class."""
-        ExecuteHelper.debug = request.config.getoption("--debug-output")
-
-    @staticmethod
-    def _print(data):
-        # method provided for debug support. Ordinarily this does nothing.
-        # Uncomment the following line to get debug output.
-        if ExecuteHelper.debug:
-            print(data)
-
-    @staticmethod
-    def execute_command(command, timeout=None):
-        """Execute the provided command list.
-
-        Executes the command and returns the error code, stdout and stderr.
-        """
-        ExecuteHelper._print("execute_command start:")
-        ExecuteHelper._print("command:")
-        ExecuteHelper._print(command)
-        p = subprocess.Popen(
-            command,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            bufsize=-1,
-            universal_newlines=True,
-        )
-        try:
-            output, error = p.communicate(timeout=timeout)
-        except subprocess.TimeoutExpired:
-            ExecuteHelper._print("Timeout after {}".format(timeout))
-            p.kill()
-            output, error = p.communicate()
-
-        ExecuteHelper._print("output:")
-        ExecuteHelper._print(output)
-        ExecuteHelper._print("error:")
-        ExecuteHelper._print(error)
-        ExecuteHelper._print("returnCode:")
-        ExecuteHelper._print(p.returncode)
-        ExecuteHelper._print("execute_command done.")
-
-        return p.returncode, output, error
-
-    @staticmethod
-    def send_mbl_cli_command(command, addr):
-        """Execute the provided mbl-cli command.
-
-        Executes the mbl-cli command and returns the error code, stdout and
-        stderr.
-        """
-        err = 1
-        output = ""
-        error = ""
-
-        if addr != "":
-            cli_command = ["mbl-cli", "--address", addr]
-            for item in command:
-                cli_command.append(item)
-            err, output, error = ExecuteHelper.execute_command(cli_command)
-        return err, output, error
+    Returns the testinfo data as a dictionary
+    """
+    return download_from_url(
+        request.config.getoption("--update-payload-testinfo-url")
+    )
 
 
+@pytest.fixture
+def update_method(request):
+    """Fixture for --update-method."""
+    return request.config.getoption("--update-method")
+
+
+# DUT fixtures
 @pytest.fixture
 def execute_helper(request):
     """Fixture to return the helper class."""
@@ -239,43 +219,5 @@ def execute_helper(request):
 
 @pytest.fixture
 def dut_addr(dut, execute_helper):
-    """Fixture to calculate the DUT IP address."""
-    dut_addr = ""
-
-    return_code, list_output, error = execute_helper.execute_command(
-        ["mbl-cli", "list"]
-    )
-
-    # The default value for dut is set to auto. This means use the first
-    # device returned by the mbl-cli. In the test farm set up there is a
-    # one-to-one mapping of device to container, so this is sufficient.
-    # However, allowing a dut to be specified allows manual testing when
-    # there is more than one device detectable on the network.
-    #
-    # The mbl-cli list command returns data in the following format:
-    #     Discovering devices. This will take up to 30 seconds.
-    #     1: mbed-linux-os-2075: fe80::21f:7bff:fe86:a738%enp0s31f6
-    #     2: mbed-linux-os-158: fe80::ecfb:3fff:fece:9376%enp0s20f0u14u1
-    #
-    # The regular expressions below extract the IP address and interface
-    # name as that is used to communicate to the DUT.
-
-    if dut == "auto":
-        # Take the first found device and use that
-        pattern = re.compile(r"\s*1:[^:]+:\s+(?P<address>\S+)")
-        for line in list_output.splitlines():
-            match = pattern.match(line)
-            if match:
-                dut_addr = match.group("address")
-                break
-    else:
-        # Match a device against the provided dut parameter
-        pattern = re.compile(r"\s*.:[^:]+:\s+(?P<address>\S+)")
-        for line in list_output.splitlines():
-            if dut in line:
-                match = pattern.match(line)
-                if match:
-                    dut_addr = match.group("address")
-                    break
-
-    return dut_addr
+    """Fixture to return the DUT address."""
+    return get_dut_address(dut, execute_helper)
