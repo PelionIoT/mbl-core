f691b762ed8748063a0130ceb81cce33883965ef
Rob Walton <rob.walton@arm.com>
2019-11-15 16:28:15 +0000

    update: Add swupdate rootfs handler
    Convert our current rootfs installer to C so we can link it with
    swupdate and have access to swupdate's utility headers.
    
    This commit will:
    * Add a CMake project to compile the handlers to object files
    * Add rootfs-handler.c - our rootfs installer
    * Add arm-handler-common - a library containing utility functions for use
    in our installers.
    
    JIRA: IOTMBL-2586

---
+++ firmware-management/swupdate-handlers/arm-handler-common.c
@@ -0,0 +1,177 @@
+#include <mntent.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include "util.h"
+#include <arm-handler-common.h>
+
+char *str_new(const size_t size)
+{
+    char *dst;
+    if (!(dst = malloc(sizeof dst * size)))
+    {
+        ERROR("%s", "Failed to allocate string");
+        return NULL;
+    }
+    return dst;
+}
+
+void str_delete(char *str)
+{
+    free(str);
+}
+
+char *str_copy(const char *src)
+{
+    if (!src)
+        return NULL;
+
+    char *dst = str_new(strlen(src) + 1);
+    if (!dst)
+    {
+        return NULL;
+    }
+
+    if (!strncpy(dst, src, strlen(src)))
+    {
+        ERROR("%s", "Failed to copy str");
+        str_delete(dst);
+        return NULL;
+    }
+
+    if (dst[strlen(src)] != '\0')
+        dst[strlen(src)] = '\0';
+
+    return dst;
+}
+
+int str_endswith(const char *const substr, const char *const fullstr)
+{
+    const size_t endlen = strlen(fullstr) - strlen(substr);
+    if (endlen <= 0)
+        return 1;
+    return strncmp(&fullstr[endlen], substr, strlen(substr));
+}
+
+char *read_file_to_new_str(const char *const filepath)
+{
+    char *return_val;
+    struct stat stat_buf;
+    if (stat(filepath, &stat_buf) == -1)
+    {
+        ERROR("%s %s: %s", "Failed to stat file", filepath, strerror(errno));
+        return NULL;
+    }
+
+    char *dst = str_new(stat_buf.st_size + 1);
+    if (!dst)
+    {
+        ERROR("%s", "Failed to allocate string buffer");
+        return NULL;
+    }
+
+    FILE *fp = fopen(filepath, "r");
+    if (fp == NULL)
+    {
+        ERROR("%s: %s", "Failed to open file", strerror(errno));
+        return_val = NULL;
+        goto clean;
+    }
+
+    fread(dst, 1, stat_buf.st_size, fp);
+    if (feof(fp) != 0 || ferror(fp) != 0)
+    {
+        ERROR("%s", "Reading from file stream failed");
+        return_val = NULL;
+        goto clean;
+    }
+
+    if (dst[stat_buf.st_size] != '\0')
+        dst[stat_buf.st_size] = '\0';
+    return_val = dst;
+
+clean:
+    if (fp != NULL)
+    {
+        if (fclose(fp) == -1)
+            return_val = NULL;
+    }
+
+    if (return_val == NULL)
+        str_delete(dst);
+
+    return return_val;
+}
+
+int get_mounted_device_path(char *dst, const char *const mount_point)
+{
+    int return_val = 1;
+
+    FILE *mtab = setmntent("/etc/mtab", "r");
+    struct mntent *mntent_desc;
+
+    while ((mntent_desc = getmntent(mtab)))
+    {
+        if (strcmp(mount_point, mntent_desc->mnt_dir) == 0)
+        {
+            if (!strncpy(dst, mntent_desc->mnt_fsname, strlen(mntent_desc->mnt_fsname)))
+            {
+                return_val = 1;
+                goto end;
+            }
+
+            dst[strlen(mntent_desc->mnt_fsname)] = '\0';
+            return_val = 0;
+            goto end;
+        }
+    }
+
+end:
+    endmntent(mtab);
+    return return_val;
+}
+
+int find_target_bank(char *dst
+                     , const char *const mounted_device
+                     , const char *const bank1_part_num
+                     , const char *const bank2_part_num)
+{
+    int return_value = 1;
+
+    char *mnt_device_cpy = str_copy(mounted_device);
+    if (!mnt_device_cpy)
+    {
+        return 1;
+    }
+
+    static const char *const delim = "p";
+    char *token = strtok(mnt_device_cpy, delim);
+    if (token == NULL)
+    {
+        ERROR("%s %s %s %s", "Failed to find", delim, "in string", mnt_device_cpy);
+        return_value = 1;
+        goto free;
+    }
+
+    if (str_endswith(bank1_part_num, mounted_device) == 0)
+    {
+        snprintf(dst, strlen(token) + strlen(delim) + strlen(bank2_part_num) + 1, "%s%s%s", token, delim, bank2_part_num);
+        return_value = 0;
+    }
+    else if (str_endswith(bank2_part_num, mounted_device) == 0)
+    {
+        snprintf(dst, strlen(token) + strlen(delim) + strlen(bank1_part_num) + 1, "%s%s%s", token, delim, bank1_part_num);
+        return_value = 0;
+    }
+    else
+    {
+        ERROR("%s %s %s %s %s %s", "Failed to find partition number", bank1_part_num, "or", bank2_part_num, "in device file", mounted_device);
+    }
+
+free:
+    str_delete(mnt_device_cpy);
+    return return_value;
+}
+
---
+++ firmware-management/swupdate-handlers/rootfs-handler.c
@@ -0,0 +1,211 @@
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "arm-handler-common.h"
+#include "handler.h"
+#include "swupdate.h"
+#include "util.h"
+
+int get_part_info_filepath(char *dst, const char *const file_name)
+{
+    static const char *const part_info_dir = "part-info";
+    return snprintf(
+            dst
+            , strlen(FACTORY_CONFIG_PARTITION) + strlen(file_name) + strlen(part_info_dir) + 3
+            ,  "%s/%s/%s"
+            , FACTORY_CONFIG_PARTITION
+            , part_info_dir
+            , file_name);
+}
+
+
+int get_target_device_file_path(char *target_device_filepath, const char *const mounted_device_filepath)
+{
+    int return_value = 0;
+
+    char const bank1_pnum_filepath[PATH_MAX];
+    char const bank2_pnum_filepath[PATH_MAX];
+    if (get_part_info_filepath(bank1_pnum_filepath, "MBL_ROOT_FS_PART_NUMBER_BANK1") < 0)
+    {
+        ERROR("Failed to create part-info file path for MBL_ROOT_FS_PART_NUMBER_BANK1");
+        return -1;
+    }
+
+    if (get_part_info_filepath(bank2_pnum_filepath,"MBL_ROOT_FS_PART_NUMBER_BANK2") < 0)
+    {
+        ERROR("Failed to create part-info file path for MBL_ROOT_FS_PART_NUMBER_BANK2");
+        return -1;
+    }
+
+    char *b1_pnum;
+    char *b2_pnum;
+    if (!(b1_pnum = read_file_to_new_str(bank1_pnum_filepath)))
+    {
+        ERROR("%s%s", "Failed to read file: ", bank1_pnum_filepath);
+        return_value = -1;
+        goto free;
+    }
+
+    if (!(b2_pnum = read_file_to_new_str(bank2_pnum_filepath)))
+    {
+        ERROR("%s%s", "Failed to read file: ", bank2_pnum_filepath);
+        return_value = -1;
+        goto free;
+    }
+
+    if (find_target_bank(target_device_filepath, mounted_device_filepath, b1_pnum, b2_pnum) != 0)
+    {
+        ERROR("%s", "Failed to find target partition.");
+        return_value = -1;
+    }
+
+free:
+    str_delete(b1_pnum);
+    str_delete(b2_pnum);
+    return return_value;
+}
+
+int copy_image_to_block_device(struct img_type *img, const char *const device_filepath)
+{
+    int ret_val = 0;
+    int fd = openfileoutput(device_filepath);
+    if (fd < 0)
+    {
+        ERROR("%s%s", "Failed to open output file ", device_filepath);
+        return -1;
+    }
+
+    const int ret = copyimage(&fd, img, NULL);
+    if (ret < 0)
+    {
+        ERROR("%s%s", "Failed to copy image to target device");
+        goto close;
+        ret_val = -1;
+    }
+
+    if (fsync(fd) == -1)
+    {
+        WARN("%s: %s", "Failed to sync filesystem", strerror(errno));
+    }
+
+close:
+    if (close(fd) == -1)
+    {
+        ERROR("Failed to close file descriptor");
+        ret_val = -1;
+    }
+
+    return ret_val;
+}
+
+int write_bootflags_file(const char *const target_device)
+{
+    char const bank1_pnum_filepath[PATH_MAX];
+    char const bank2_pnum_filepath[PATH_MAX];
+
+    if (get_part_info_filepath(bank1_pnum_filepath, "MBL_ROOT_FS_PART_NUMBER_BANK1") < 0)
+    {
+        ERROR("Failed to create part-info file path for MBL_ROOT_FS_PART_NUMBER_BANK1");
+        return -1;
+    }
+
+    if (get_part_info_filepath(bank2_pnum_filepath,"MBL_ROOT_FS_PART_NUMBER_BANK2") < 0)
+    {
+        ERROR("Failed to create part-info file path for MBL_ROOT_FS_PART_NUMBER_BANK2");
+        return -1;
+    }
+
+    int ret_val = 0;
+    char *b1_pnum;
+    char *b2_pnum;
+    if (!(b1_pnum = read_file_to_new_str(bank1_pnum_filepath)))
+    {
+        return -1;
+    }
+
+    if (!(b2_pnum = read_file_to_new_str(bank2_pnum_filepath)))
+    {
+        goto free;
+        ret_val = -1;
+    }
+
+    if (mkdir(BOOTFLAGS_DIR, 0755) == -1)
+    {
+        if (errno != EEXIST)
+        {
+            ret_val = -1;
+            goto free;
+        }
+    }
+
+    if (str_endswith(b2_pnum, target_device) == 0)
+    {
+        char bootflags_file_path[PATH_MAX];
+        snprintf(bootflags_file_path, BOOTFLAGS_DIR, "rootfs2");
+
+        FILE* file;
+        if (!(file = fopen(bootflags_file_path, "w")))
+        {
+            ret_val = -1;
+            goto free;
+        }
+
+        if (fprintf(file, "") == -1)
+        {
+            ret_val = -1;
+        }
+        fclose(file);
+    }
+
+free:
+    str_delete(b1_pnum);
+    str_delete(b2_pnum);
+    return ret_val;
+}
+
+int rootfs_handler(struct img_type *img
+                   , void __attribute__ ((__unused__)) *data)
+{
+    static const char *const root_mnt_point = "/";
+    char mounted_device_filepath[PATH_MAX];
+    if (get_mounted_device_path(mounted_device_filepath, root_mnt_point) != 0)
+    {
+        ERROR("%s", "Failed to get mounted device file path.");
+        return 1;
+    }
+
+    char target_device_filepath[PATH_MAX];
+    if (get_target_device_file_path(target_device_filepath, mounted_device_filepath) != 0)
+    {
+        ERROR("%s", "Failed to get target device file path");
+        return 1;
+    }
+
+    if (copy_image_to_block_device(img, target_device_filepath) == -1)
+    {
+        ERROR("%s %s %s %s", "Failed to copy image", img->fname, "to device", target_device_filepath);
+        return 1;
+    }
+
+    if (write_bootflags_file(target_device_filepath) == -1)
+    {
+        ERROR("Failed to write boot flag file. Next boot will be from bank 1");
+        return 1;
+    }
+
+    return 0;
+}
+
+__attribute__((constructor))
+void rootfs_handler_init(void)
+{
+    register_handler("ROOTFSv4"
+                     , rootfs_handler
+                     , IMAGE_HANDLER
+                     , NULL);
+}
+
