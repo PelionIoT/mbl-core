dfba58f81330b54a0c07545b86107f2ea9edf729
Pete Dyer <pete.dyer@arm.com>
2019-11-22 14:37:08 +0000

    Update test to use local.conf file.
    First - make the test work without sfdisk.

--- mbl-core/tests/partitions/test_partitions.py
+++ mbl-core/tests/partitions/test_partitions.py
@@ -8,7 +8,7 @@ Pytest for checking partition offsets and sizes.
 
 The main idea is to create two lists of (offset, size) pairs:
 * one based on partition data left in the factory config partition;
-* one based on the output of "sfdisk -J".
+* one based on the content of /sys/block.
 
 We then check that the two lists are equal.
 """
@@ -16,9 +16,9 @@ We then check that the two lists are equal.
 import pathlib
 import re
 import subprocess
-import json
 
-PART_INFO_DIR = pathlib.Path("/config/factory/part-info")
+EXPECTED_PART_INFO_DIR = pathlib.Path("/config/factory/part-info")
+ACTUAL_PART_INFO_DIR = pathlib.Path("/sys/block")
 
 
 def get_var_for_part(var_name, part_name, default=None):
@@ -35,7 +35,7 @@ def get_var_for_part(var_name, part_name, default=None):
     and partition.
     """
     filename = "MBL_{}_{}".format(re.escape(part_name), re.escape(var_name))
-    path = PART_INFO_DIR / filename
+    path = EXPECTED_PART_INFO_DIR / filename
     if default is not None and not path.is_file():
         return default
     return path.read_text()
@@ -47,7 +47,7 @@ def get_fs_part_names():
     """
     part_names = []
     filename_re = re.compile(r"^MBL_([A-Z0-9_]+)_SIZE_KiB$")
-    for f in PART_INFO_DIR.iterdir():
+    for f in EXPECTED_PART_INFO_DIR.iterdir():
         filename_match = filename_re.match(f.name)
         if filename_match:
             part_name = filename_match.group(1)
@@ -86,9 +86,12 @@ def get_device_file_for_parts():
 
     This function assumes that all of the partitions are on the same device.
     """
-    return subprocess.check_output(["blkid", "-L", "rootfs1"]).rsplit(b"p", 1)[
-        0
-    ]
+    return str(
+        subprocess.check_output(["blkid", "-L", "rootfs1"])
+        .rsplit(b"p", 1)[0]
+        .split(b"/")[2],
+        "utf-8",
+    )
 
 
 def get_actual_part_table():
@@ -96,34 +99,50 @@ def get_actual_part_table():
     Get the actual partition table.
 
     Returns a list of (offset, size) pairs for file system partitions, based on
-    the output of "sfdisk -J".
+    the content of /sys/block.
+
 
     The list is sorted and the offsets and sizes are in KiB units.
     """
     device_file = get_device_file_for_parts()
-    # "sfdisk -J" gives JSON output
-    sfdisk_json = subprocess.check_output(["sfdisk", "-J", device_file])
+    device_dir = pathlib.Path.joinpath(ACTUAL_PART_INFO_DIR, device_file)
 
-    # The output contains some info we're not interested in - we just want the
-    # "partitions" list which contains dicts with an offsets and sizes for each
-    # partition.
-    sfdisk_part_table = json.loads(sfdisk_json)["partitiontable"]["partitions"]
+    part_table = []
 
-    # Convert the partition table from sfdisk into a list of (offset, size)
-    # tuples. Divide the numbers by 2 to convert 512B sectors that sfdisk uses
-    # into KiB
-    part_table = sorted(
-        [
-            (int(d["start"]) // 2, int(d["size"]) // 2)
-            for d in sfdisk_part_table
-        ]
-    )
+    filename_re = re.compile(r"^.*{}p([0-9]+)$".format(device_file))
+
+    for f in device_dir.iterdir():
+        filename_match = filename_re.match(f.name)
+        if filename_match:
+            size_KiB = int(get_var_for_actual_part("size", f))
+            offset1_KiB = int(get_var_for_actual_part("start", f))
+            part_table.append((offset1_KiB // 2, size_KiB // 2))
+
+    part_table = sorted(part_table)
 
     # The fourth partition is an "extended partition" that just contains other
     # partitions. Ignore it.
     del part_table[3]
 
     return part_table
+
+
+def get_var_for_actual_part(var_name, part_name, default=None):
+    """
+    Get the value of a partition config variable for a partition.
+
+    Args:
+    * var_name (str): name of partition property to get.
+    * part_name (str): name of partition.
+
+    Returns (str): the contents of the config file for the given variable name
+    and partition, or empty string if the config file doesn't exist.
+    """
+    path = part_name / var_name
+    if not path.is_file():
+        return ""
+    return path.read_text()
+
 
 
 def test_part_table():
