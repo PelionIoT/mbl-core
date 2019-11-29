7b7ca5de89aac18094e9f204fd46a48260f75a37
Rob Walton <rob.walton@arm.com>
2019-11-29 16:12:59 +0000

    update: Add minimal UpdateD
    This commit adds a minimal update daemon which just starts and spins in
    the background doing nothing. Add a cmake project to build the
    executable and add a cpp file which does absolutely nothing for the
    moment.
    
    JIRA: IOTMBL-2649

---
+++ firmware-management/updated/updated.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * UpdateD (pronounced update-dee) is a system daemon that coordinates updates
+ * to device firmware.  Updates will consist of bundles of one or more
+ * firmware packages that should be applied as a set.  The delivery of updates
+ * to a device will be the responsibility of an external delivery mechanism.
+ * UpdateD runs in the background on a device and listens for update requests.
+ * When a request is received, UpdateD will hand off responsibility for applying
+ * the update to swupdate. */
+
+int main()
+{
+    while(1)
+    {
+        continue;
+    }
+    return 0;
+}
