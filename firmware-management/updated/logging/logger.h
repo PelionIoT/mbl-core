0ee01f829b33bdc313df20c28174e648faba3d91
Rob Walton <rob.walton@arm.com>
2019-12-18 21:51:36 +0000

    use a namespace alias for spdlog

--- firmware-management/updated/logging/logger.h
+++ firmware-management/updated/logging/logger.h
@@ -1,13 +1,13 @@
 #ifndef UPDATED_LOGGER_H
 #define UPDATED_LOGGER_H
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/systemd_sink.h>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/systemd_sink.h"
 
 
 namespace updated
 {
-    using logger = spdlog;
+    namespace logger = spdlog;
     auto systemd_sink = std::make_shared<logger::sinks::systemd_sink_st>();
     logger::logger logger("updated_logger", systemd_sink);
     logger::set_default_logger(logger);
